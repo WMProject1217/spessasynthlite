9ef64baf677f72a06a7da22c331dd5bb50383ab4
WMProject1217 <72447462+WMProject1217@users.noreply.github.com>
2026-06-25 15:03:53 +0800

    Delete examples directory

--- examples/examples_common.js
+++
@@ -1,3 +0,0 @@
-export const EXAMPLE_SOUND_BANK_PATH =
-    "https://spessasus.github.io/SpessaSynth/soundfonts/GeneralUserGS.sf3";
-export const EXAMPLE_WORKLET_PATH = "./spessasynth_processor.min.js";
